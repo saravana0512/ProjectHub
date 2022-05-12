a5e4732585f8dafc5ceffcd10e2c78e414d2a540
saravana0512 <saravanasjob@gmail.com>
2022-05-12 12:21:38 +0530

    Third Update

--- src/org/prac/Practice.java
+++ src/org/prac/Practice.java
@@ -4,6 +4,8 @@ public class Practice {
 
 	public static void main(String[] args) {
 		System.out.println("Id=123456");
+		System.out.println("phno=13344662");
+		System.out.println("add=159");
 
 	}
 
